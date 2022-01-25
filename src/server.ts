434f93b6fb872868366dd8cd3b267f8c214ea68e
Abel Souza <abelsouzacosta@gmail.com>
2022-01-24 21:50:16 -0300

    chore(hello-world): initial configuration for project

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) =>
+  res.status(200).json({
+    message: "Olá mundo",
+  })
+);
+
+const PORT = 3333;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
