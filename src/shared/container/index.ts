7843fb98ffeab488a18a6e63950fe9ee9f19ac86
Abel Souza <abelsouzacosta@gmail.com>
2022-01-25 14:03:50 -0300

    feat(product): container
    adding singleton container for ProductRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { ICategoryRepository } from "src/modules/categories/repositories/categories/ICategoryRepository";
 import { CategoryRepository } from "src/modules/categories/repositories/implementations/CategoryRepository";
+import { ProductRepository } from "src/modules/products/repositories/implementations/ProductRepository";
+import { IProductRepository } from "src/modules/products/repositories/products/IProductRepository";
 import { container } from "tsyringe";
 
 container.registerSingleton<ICategoryRepository>(
   "CategoryRepository",
   CategoryRepository
+);
+
+container.registerSingleton<IProductRepository>(
+  "ProductRepository",
+  ProductRepository
 );
