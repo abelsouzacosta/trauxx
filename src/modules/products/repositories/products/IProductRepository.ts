eaa0393acfbcf2a945677d55891c83784dcc1fd8
Abel Souza <abelsouzacosta@gmail.com>
2022-01-25 13:53:04 -0300

    feat(product): repository
    setting repository interface

---
+++ src/modules/products/repositories/products/IProductRepository.ts
@@ -0,0 +1,29 @@
+import { Product } from "../../entities/Product";
+
+interface ICreateProductDTO {
+  name: string;
+  image: string;
+  category_id: number;
+}
+
+interface IUpdateProductDTO extends ICreateProductDTO {
+  id: number;
+}
+
+interface IProductRepository {
+  findById(id: number): Promise<Product>;
+
+  findByName(name: string): Promise<Product>;
+
+  findByCategory(category_id: number): Promise<Product[]>;
+
+  list(): Promise<Product[]>;
+
+  create({ name, image, category_id }: ICreateProductDTO): Promise<void>;
+
+  update({ id, name, image, category_id }: IUpdateProductDTO): Promise<void>;
+
+  delete(id: number): Promise<void>;
+}
+
+export { IProductRepository };
