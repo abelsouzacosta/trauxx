3ace2a312d4f8b04d5deb4907575d835575abb3c
Abel Souza <abelsouzacosta@gmail.com>
2022-01-24 22:45:31 -0300

    feat(database): adding migration

---
+++ src/database/migrations/1643073626022-CreateCategory.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategory1643073626022 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "categories",
+        columns: [
+          {
+            name: "id",
+            type: "integer",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "image",
+            type: "varchar",
+            isNullable: true,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories");
+  }
+}
